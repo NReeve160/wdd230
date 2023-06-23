cfff0c6c384a35fd4f514605ebc47e10cf53f99d
Mc_Orc <reeve.nathan160@gmail.com>
2023-06-23 09:00:28 -0600

    created dom manipulation page

---
+++ lesson5/bom.js
@@ -0,0 +1,31 @@
+const list = document.querySelector("#list");
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+
+      button.addEventListener('click', () => {
+        if (input.value.length == 0)
+        {
+            alert("Please put something in the box")
+            return false;
+        }
+
+        const myItem = input.value;
+        input.value = '';
+
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
+
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listBtn);
+        listBtn.textContent = '❌';
+        list.appendChild(listItem);
+
+        listBtn.addEventListener('click', () => {
+          list.removeChild(listItem);
+        });
+
+        input.focus();
+
+    });
