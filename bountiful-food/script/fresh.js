697147e940843057000aacfb299822a3011f782d
NReeve160 <reeve.nathan160@gmail.com>
2023-07-19 21:28:02 -0600

    leedle leeedle leedle

--- bountiful-food/script/fresh.js
+++ bountiful-food/script/fresh.js
@@ -1,5 +1,7 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/fruit.json';
 
+const test = document.querySelector('.test');
+
 
   async function getFruitData() {
     const response = await fetch(url);
@@ -8,4 +10,30 @@ const url = 'https://brotherblazzard.github.io/canvas-content/fruit.json';
     displayFruits(data);
   }
 
-  const random = displayFruits();
+  getFruitData();
+
+  const displayFruits = (data) => {
+    const cards1 = document.querySelector('#fruit1');
+    const cards2 = document.querySelector('#fruit2');
+    const cards3 = document.querySelector('#fruit3');
+
+    for (var i = 0; i <= data.length; i++) {
+    let option1 = document.createElement('option');
+    let option2 = document.createElement('option');
+    let option3 = document.createElement('option');
+
+
+    option1.textContent = `${data[i].name}`
+    option2.textContent = `${data[i].name}`
+    option3.textContent = `${data[i].name}`
+
+
+    cards1.appendChild(option1);
+    cards2.appendChild(option2);
+    cards3.appendChild(option3);
+    };
+
+    console.log(cards1);
+    console.log(cards2);
+    console.log(cards3);
+  }
